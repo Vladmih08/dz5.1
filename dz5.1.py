25b8bf9ec49a8e049e5b5f658598cd3eda3d3264
Vladmih08 <vladmih2008@gmail.com>
2025-07-01 13:00:09 +0300

    Виправив, тепер повинно працювати

--- dz5.1.py
+++ dz5.1.py
@@ -5,7 +5,7 @@ import keyword
 a = input()
 rezultat = "True"
 #---------------------------------------------------------------------------#
-if a[0].isdigit() or a in keyword.kwlist or a.count('_') > 1:
+if a[0].isdigit() or a in keyword.kwlist or '__' in a:
     rezultat = "False"
 for simvol in a:
     if simvol in string.punctuation and simvol != '_' or simvol.isupper():
